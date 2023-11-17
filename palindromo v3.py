f008848af6b4189d8990662880d6977b29fb80b6
rocapao11 <150872979+rocapao11@users.noreply.github.com>
2023-11-17 16:28:46 -0600

    Update palindromo v3.py

--- palindromo v3.py
+++ palindromo v3.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 16 13:50:58 2023
-
-@author: and_p
-"""
 #V3 una frase es palindromo
 print ("Este programa te indica si un texto es o no un palindomo")
 def no_space(texto): #definimos una funcion para eliminar los espacios escritos
